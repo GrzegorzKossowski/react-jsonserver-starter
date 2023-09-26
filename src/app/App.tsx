8af3fc598c87f7fa1aaa5afb53f8af50b0dba690
Grzegorz Kossowski <grzegorz.kossowski@gmail.com>
2023-09-26 07:38:32 +0200

    first commit

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return (
+        <main>
+            <div className='text-red-500'>Red text</div>
+        </main>
+    );
+}
+
+export default App;
